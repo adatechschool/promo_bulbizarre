0b48dcc4eff0292ddea558799ccb54908fcd9a8a
Jane Lane <loan.does@gmail.com>
2020-04-21 17:23:22 +0200

    bordure noire, titre bleu, p rouge

--- extension-navigateur/2020-04-20/Del_Loan/plugin_01.js
+++ extension-navigateur/2020-04-20/Del_Loan/plugin_01.js
@@ -1,3 +1,14 @@
-document.body.style.border = "5px solid red";
-document.body.style.color = "blue";
-document.image.style.border = "5px solid green";
+
+document.body.style.border = "5px solid black";
+
+let h1 = document.getElementsByTagName("h1");
+for (elt of h1) {
+	elt.style.color = "blue";
+}
+
+let p = document.getElementsByTagName("p");
+	for (elt of p) {
+	elt.style.color = "red";
+}
+
+// document.getElementByTagName("img").body.style.border = "5px solid red";
