d3dca6aeac48428c9c7110bed40782d8603a6c07
Wara-nyu <w.bounkeua@gmail.com>
2020-04-28 17:18:07 +0200

    ajout de l'heure lors du chargement

--- dataviz/2020-04-27/soumayatwara/script.js
+++ dataviz/2020-04-27/soumayatwara/script.js
@@ -1,3 +1,7 @@
+function AddZero(num) {
+  return (num >= 0 && num < 10) ? "0" + num : num + "";
+}
+
 window.onload = function(){ // permet d'afficher le canvas dés qu'on ouvre le canvas
 
   /*  var canvas = document.createElement('canvas'); // on déclare une variable pour utiliser l'element qui va permettre de dessiner le canvas .
@@ -6,7 +10,7 @@ window.onload = function(){ // permet d'afficher le canvas dés qu'on ouvre le c
   canvas.style.border = "1px solid";
   document.body.appendChild(canvas);
 
-  */
+  
   console.log('je suis dans la fonction');
   let canvas = document.getElementById('c1');
   let ctx = canvas.getContext('2d');
@@ -19,5 +23,14 @@ window.onload = function(){ // permet d'afficher le canvas dés qu'on ouvre le c
   }
 
   image.addEventListener('load', drawingImage, false);
-  console.log('fin de la fonction');
-}
+  console.log('fin de la fonction');*/
+
+  //function heure() {
+    var now = new Date();
+    var strDateTime = [ 
+        [AddZero(now.getHours()), 
+        AddZero(now.getMinutes()),
+        AddZero(now.getSeconds())].join(":")].join(" ");
+    document.getElementById("Console").innerHTML = strDateTime;
+//}
+};
