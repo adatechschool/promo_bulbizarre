96b8de928f4735d52c80904d6482d9bd172d71e8
Sooumayat <soumayat.hamadi@hotmail.com>
2020-04-29 16:39:22 +0200

    nettoie le code

--- dataviz/2020-04-27/soumayatwara/script.js
+++ dataviz/2020-04-27/soumayatwara/script.js
@@ -4,39 +4,23 @@ function AddZero(num) {
 
 window.onload = function(){ // permet d'afficher le canvas dés qu'on ouvre le canvas
 
-  /*  var canvas = document.createElement('canvas'); // on déclare une variable pour utiliser l'element qui va permettre de dessiner le canvas .
-  canvas.width = 900;
-  canvas.height = 600;
-  canvas.style.border = "1px solid";
-  document.body.appendChild(canvas);
-
-
-  console.log('je suis dans la fonction');
-  let canvas = document.getElementById('c1');
-  let ctx = canvas.getContext('2d');
-  let image = document.getElementById('arbre');
-  let drawingImage = function(){
-    console.log('je suis avant le canvas.drawImage');
-    canvas.drawImage(image, 75, 25, 800, 500);
-
-    console.log('je suis dans la fonction drawingImage');
-  }
-
-  image.addEventListener('load', drawingImage, false);
-  console.log('fin de la fonction');*/
-
-  /*var c = document.getElementById("c1");
+  var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
   var img = document.getElementById("image");
-  ctx.drawImage(img, 100, 100);*/
+  ctx.drawImage(img , 0, 0, 880, 580);
 
 
-  //function heure() {
+
     var now = new Date();
     var strDateTime = [
         [AddZero(now.getHours()),
         AddZero(now.getMinutes()),
         AddZero(now.getSeconds())].join(":")].join(" ");
     document.getElementById("Console").innerHTML = strDateTime;
-//}
+
+    ctx.beginPath();
+    ctx.arc(150, 106, 50, 0, Math.PIx2, false);
+    ctx.fillStyle = 'rgb(0, 0, 255)';
+    ctx.fill();
+    ctx.closePath();
 };
