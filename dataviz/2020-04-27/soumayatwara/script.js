783a5d171fc1bd668110a65d5a977792e42c250f
Sooumayat <soumayat.hamadi@hotmail.com>
2020-04-29 15:31:44 +0200

    ajoute l'image dans le canvas

--- dataviz/2020-04-27/soumayatwara/script.js
+++ dataviz/2020-04-27/soumayatwara/script.js
@@ -10,7 +10,7 @@ window.onload = function(){ // permet d'afficher le canvas dés qu'on ouvre le c
   canvas.style.border = "1px solid";
   document.body.appendChild(canvas);
 
-  
+
   console.log('je suis dans la fonction');
   let canvas = document.getElementById('c1');
   let ctx = canvas.getContext('2d');
@@ -25,10 +25,16 @@ window.onload = function(){ // permet d'afficher le canvas dés qu'on ouvre le c
   image.addEventListener('load', drawingImage, false);
   console.log('fin de la fonction');*/
 
+  var c = document.getElementById("c1");
+  var ctx = c.getContext("2d");
+  var img = document.getElementById("image");
+  ctx.drawImage(img, 100, 100);
+
+
   //function heure() {
     var now = new Date();
-    var strDateTime = [ 
-        [AddZero(now.getHours()), 
+    var strDateTime = [
+        [AddZero(now.getHours()),
         AddZero(now.getMinutes()),
         AddZero(now.getSeconds())].join(":")].join(" ");
     document.getElementById("Console").innerHTML = strDateTime;
