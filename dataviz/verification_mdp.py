551b04733a715bd4a75502c947c944767e1e4cab
Zakia <chabanezakia@gmail.com>
2020-05-05 14:25:47 +0200

    ajouter fenetre saisie mdp en tkinter

---
+++ dataviz/verification_mdp.py
@@ -0,0 +1,43 @@
+# script mot_de_passe.py
+from tkinter import *
+from tkinter.messagebox import * # boîte de dialogue
+
+def Verification():
+    if Motdepasse.get() == 'Lydiajaipasmislebonmdp:wq':#rentre le bon mot de passe pour tester
+        # le mot de passe est bon : on affiche une boîte de dialogue puis on ferme la fenêtre
+        showinfo('Résultat','Mot de passe correct.\nAu revoir !')
+        Mafenetre.destroy()
+    else:
+        # le mot de passe est incorrect : on affiche une boîte de dialogue
+        showwarning('Résultat','Mot de passe incorrect.\nVeuillez recommencer !')
+        Motdepasse.set('')
+
+# Création de la fenêtre principale (main window)
+Mafenetre = Tk()
+Mafenetre.title('Identification requise')
+
+# Création d'un widget Label (texte 'Mot de passe')
+Label1 = Label(Mafenetre, text = 'Mot de passe ')
+Label1.pack(side = LEFT, padx = 5, pady = 5)
+
+# Création d'un widget Entry (champ de saisie)
+Motdepasse= StringVar()
+Champ = Entry(Mafenetre, textvariable= Motdepasse, show='*', bg ='bisque', fg='maroon')
+Champ.focus_set()
+Champ.pack(side = LEFT, padx = 5, pady = 5)
+
+def recupere():
+    showinfo("Alerte",entree.get())
+value=StringVar()
+value.set("Valeur")
+entree = Entry(Mafenetre,textvariable=value,width=30)
+entree.pack() 
+
+bouton = Button(Mafenetre,text="Valider",command=recupere)
+bouton.pack()
+
+# Création d'un widget Button (bouton Valider)
+Bouton = Button(Mafenetre, text ='Valider', command = Verification)
+Bouton.pack(side = LEFT, padx = 5, pady = 5)
+
+Mafenetre.mainloop()
