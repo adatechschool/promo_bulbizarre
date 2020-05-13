bc148008dd7bba0a146c49f390c0c486ea731ec5
Wara-nyu <w.bounkeua@gmail.com>
2020-05-13 12:02:46 +0200

    cree indexHTML et un fichier js

---
+++ chatbot-cuisine/atelier individuel/Del/boucleJson.js
@@ -0,0 +1,19 @@
+const jsonURL = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json'
+
+const request = new XMLHttpRequest();
+
+resquest.open('GET', jsonURL);
+
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  const pokemon = request.response;
+  countPokemon(pokemon);
+  bigPokemon(pokemon);
+  sortWeight(pokemon);
+  evolutionPokemon(pokemon);
+}
+
+function countPokemon(jsonObj) {
+
+}
