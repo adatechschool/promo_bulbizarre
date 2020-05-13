aef68860617b4613f25be3bc7f5e8a1e51ff6c91
KheOps <kheops@ceops.eu>
2020-05-13 11:46:35 +0200

    Exemple de fonction pour ceux de plus de 10 kg
    Signed-off-by: KheOps <kheops@ceops.eu>

--- chatbot-cuisine/atelier individuel/pokemon-kheops.py
+++ chatbot-cuisine/atelier individuel/pokemon-kheops.py
@@ -16,6 +16,16 @@ def nbpokemon (data)
   return data['pokemon'].length
 end
 
+def assezlourd (data, limite)
+  ensemble = data['pokemon'].select { |p| p['weight'].to_i >= limite }
+  return ensemble
+end
+
 nbp = nbpokemon data
+plusdixkg = assezlourd data, 10
+nomsdixkg = []
+plusdixkg.each { |p| nomsdixkg << p['name'] }
+
 
 puts "Il y a #{nbp} pokemons"
+puts "Les pokemons de plus de 10 kg sont : #{nomsdixkg.join(', ')}"
