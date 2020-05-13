0e381a5aeea8d2057311ec39835b7b034deb8955
Wara-nyu <w.bounkeua@gmail.com>
2020-05-13 12:48:26 +0200

    petits corrections

--- chatbot-cuisine/atelier individuel/Del/boucleJson.js
+++ chatbot-cuisine/atelier individuel/Del/boucleJson.js
@@ -2,20 +2,21 @@ const jsonURL = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/maste
 
 const request = new XMLHttpRequest();
 
-resquest.open('GET', jsonURL);
+request.open('GET', jsonURL);
 
 request.responseType = 'json';
 request.send();
 request.onload = function() {
   const pokemon = request.response;
+  console.log(pokemon);
   countPokemon(pokemon);
-  bigPokemon(pokemon);
-  sortWeight(pokemon);
-  evolutionPokemon(pokemon);
+  //bigPokemon(pokemon);
+  //sortWeight(pokemon);
+  //evolutionPokemon(pokemon);
 }
 
 function countPokemon(jsonObj) {
-  for(i=0; i<length(pokemon[]); i++) {
-   return i
-  }
+  //for(i=0; i<length(pokemon[]); i++) {
+   //return i
+  //}
 }
