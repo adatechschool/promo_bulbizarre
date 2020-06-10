69a3bfd1448be1e5abcab533ed3b334103e20e3a
KheOps <kheops@ceops.eu>
2020-06-10 19:59:12 +0200

    Ajout vecteur d'Item à Personnage
    Signed-off-by: KheOps <kheops@ceops.eu>

--- jeu-de-rôle/code commun/Personnage.hpp
+++ jeu-de-rôle/code commun/Personnage.hpp
@@ -1,13 +1,16 @@
 #ifndef PERSONNAGE
 #define PERSONNAGE
 #include <iostream>
-
+#include <vector>
+#include "Item.hpp"
 
 class Personnage
 {
     std::string nom;
     int points_de_vie;
     int score;
+    // sprite sheet (?) apparence; // cf. diagramme de classe
+    vector<Item> inventaire;
     public:
         Personnage();
         void sePresenter();
