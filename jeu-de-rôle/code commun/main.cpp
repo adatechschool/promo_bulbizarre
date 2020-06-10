e65e07824461b8e88c8441b8c219ad6f9c754868
KheOps <kheops@ceops.eu>
2020-06-10 20:02:11 +0200

    Quelques exemples d'appel d'accesseur dans la fonction main
    Signed-off-by: KheOps <kheops@ceops.eu>

--- jeu-de-rôle/code commun/main.cpp
+++ jeu-de-rôle/code commun/main.cpp
@@ -6,6 +6,12 @@ int main()
     //créer un personnage s'il te plait ?
     Personnage monPersonnage;
     monPersonnage.sePresenter();
+
+    std::string nomDuPersonnage;
+    //nomDuPersonnage = monPersonnage.nom; // interdit car la variable
+    // d'instance 'nom' est privée dans la classe Personnage
+    nomDuPersonnage = monPersonnage.getNom();
+
     while (true) {
       
     }
